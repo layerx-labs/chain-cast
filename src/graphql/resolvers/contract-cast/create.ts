4c61c09740e3250cb5fd0da36546d69d1df6f85a
JoseMarshall <hebojosemar@gmail.com>
2023-06-15 20:42:53 +0100

    feat: refactor types

--- src/graphql/resolvers/contract-cast/create.ts
+++ src/graphql/resolvers/contract-cast/create.ts
@@ -1,58 +1,56 @@
 import { ErrorsEnum } from '@/constants/index';
+import { Resolver } from '@/graphql/types';
 import { UserInputError } from '@/middleware/errors';
-import { AppContext } from '@/types/index';
 import { ContractCast, ContractCastType } from '@prisma/client';
 
 export type CreateContractCastArgType = {
+  data: {
+    address: string;
+    type: ContractCastType;
+    chainId: number;
+    startFrom?: number;
+  };
+};
+
+const createContractCast: Resolver<ContractCast, CreateContractCastArgType> = async (
+  _1,
+  args,
+  ctx
+) => {
+  const oldContractCast = await ctx.db.contractCast.findUnique({
+    where: {
+      chainId_address: {
+        address: args.data.address,
+        chainId: args.data.chainId,
+      },
+    },
+  });
+  if (oldContractCast) {
+    throw new UserInputError('Chain Cast already found', ErrorsEnum.alreadyExists);
+  }
+
+  const contractCast = await ctx.db.contractCast.create({
     data: {
-        address: string,
-        type: ContractCastType,
-        chainId: number,
-        startFrom?: number | null | undefined,
-    },    
-}
+      address: args.data.address,
+      type: args.data.type,
+      chainId: args.data.chainId,
+      blockNumber: args.data.startFrom ?? 0,
+    },
+    select: {
+      id: true,
+      address: true,
+      blockNumber: true,
+      chainId: true,
+      createdAt: true,
+      type: true,
+    },
+  });
+  ctx.log.i(
+    `Created a new Chain Cast id ${contractCast.id} ${contractCast.chainId} ` +
+      `${contractCast.address}`
+  );
+  ctx.whisperer.addCast(contractCast);
+  return contractCast;
+};
 
-
-export default async function createContractCast(
-    _1: unknown,
-    _2: unknown,
-    args: CreateContractCastArgType,
-    ctx: AppContext
-  ): Promise<ContractCast> {
-
-    const oldContractCast = await ctx.db.contractCast.findUnique({
-        where: {
-            chainId_address: {
-                address: args.data.address,
-                chainId: args.data.chainId,
-            }
-        }
-    });
-    if (oldContractCast) {
-        throw new UserInputError(
-            'Chain Cast already found', 
-            ErrorsEnum.alreadyExists
-        );
-    }
-
-    const contractCast = await ctx.db.contractCast.create({    
-        data: {
-            address: args.data.address,
-            type: args.data.type,
-            chainId: args.data.chainId,
-            blockNumber: args.data.startFrom ?? 0,
-        },
-        select: {
-            id: true,
-            address: true,
-            blockNumber: true,
-            chainId: true,
-            createdAt: true,
-            type: true,
-        },
-    })
-    ctx.log.i(`Created a new Chain Cast id ${contractCast.id} ${contractCast.chainId} ` + 
-              `${contractCast.address}`)
-    ctx.whisperer.addCast(contractCast);
-    return contractCast;
-}
+export default createContractCast;
--- src/graphql/resolvers/contract-cast/delete.ts
+++ src/graphql/resolvers/contract-cast/delete.ts
@@ -1,38 +1,36 @@
 import { ErrorsEnum } from '@/constants/index';
+import { Resolver } from '@/graphql/types';
 import { UserInputError } from '@/middleware/errors';
-import { AppContext } from '@/types/index';
 import { ContractCast } from '@prisma/client';
 
-export async function deleteContractCast(
-    _1: unknown,
-    _2: unknown,
-    args: {
-        id: string,
-    },
-     ctx: AppContext
-  ): Promise<ContractCast> {
+type DeleteContractCastArgType = {
+  id: string;
+};
 
-    const contractCast = await ctx.db.contractCast.delete({    
-        where: {
-            id: args.id,
-        },
-        select: {
-            id: true,
-            address: true,
-            blockNumber: true,
-            chainId: true,
-            createdAt: true,
-            type: true,
-        },
-    })
-    if (!contractCast) {
-        throw new UserInputError(
-            'Chain Cast not found', 
-            ErrorsEnum.objectNotFound
-        );
-    }
-    ctx.log.i(`Deleted Chain Cast id ${contractCast.id} ${contractCast.chainId} `+ 
-             `${contractCast.address}`)
-    ctx.whisperer.deleteCast(contractCast.id);
-    return contractCast;
-}
+export const deleteContractCast: Resolver<ContractCast, DeleteContractCastArgType> = async (
+  _1,
+  args,
+  ctx
+) => {
+  const contractCast = await ctx.db.contractCast.delete({
+    where: {
+      id: args.id,
+    },
+    select: {
+      id: true,
+      address: true,
+      blockNumber: true,
+      chainId: true,
+      createdAt: true,
+      type: true,
+    },
+  });
+  if (!contractCast) {
+    throw new UserInputError('Chain Cast not found', ErrorsEnum.objectNotFound);
+  }
+  ctx.log.i(
+    `Deleted Chain Cast id ${contractCast.id} ${contractCast.chainId} ` + `${contractCast.address}`
+  );
+  ctx.whisperer.deleteCast(contractCast.id);
+  return contractCast;
+};
